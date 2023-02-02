e9e0da43f4e4798c1362cc6a4da71f5e98327d4f
Abel3581 <marzoa3581@gmail.com>
2023-02-02 19:37:18 -0300

    act

---
+++ BACKEND/src/test/java/com/c823/consorcio/auth/controller/UserAuthControllerTest.java
@@ -0,0 +1,20 @@
+package com.c823.consorcio.auth.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserAuthControllerTest {
+
+    @Test
+    void signUp() {
+    }
+
+    @Test
+    void signUpAdmin() {
+    }
+
+    @Test
+    void signIn() {
+    }
+}
