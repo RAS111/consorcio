1d7de54f21f35d3a4e27a18117392e2f0aa2ac3e
agustin-kiryk <agustin.kiryk@gmail.com>
2023-02-06 20:18:38 -0300

    4# create seeder turns, turn repository and modify Turns Enums

---
+++ BACKEND/src/main/java/com/c823/consorcio/repository/ITurnRepository.java
@@ -0,0 +1,8 @@
+package com.c823.consorcio.repository;
+
+import com.c823.consorcio.entity.TurnEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITurnRepository extends JpaRepository<TurnEntity, Long> {
+
+}
