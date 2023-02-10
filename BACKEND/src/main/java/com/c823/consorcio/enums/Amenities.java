5dea40cf3ba40a9b10aa68c7bbd4a6eb3592c09a
agustin-kiryk <agustin.kiryk@gmail.com>
2023-02-10 18:59:28 -0300

    SP1-5#: seeders amenities

--- BACKEND/src/main/java/com/c823/consorcio/enums/Amenities.java
+++ BACKEND/src/main/java/com/c823/consorcio/enums/Amenities.java
@@ -1,5 +1,14 @@
 package com.c823.consorcio.enums;
 
 public enum Amenities {
-  LOUNGE, SWPOOL, GRILL, GYM, TENNIS
+  LOUNGE ("LOUNGE"), SWPOOL("SWPOOL"), GRILL("GRILL"), GYM("GYM"), TENNIS("TENNIS");
+
+  private final String name;
+
+  Amenities(String name) { this.name = name;}
+
+  public Amenities getName(){
+
+    return Amenities.valueOf(name);
+  }
 }
